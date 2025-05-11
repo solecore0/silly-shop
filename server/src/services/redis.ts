6f1fb8fbd2bac9ae762f8901373b456713ca4e4c
super_ <120087639+abdullah-super@users.noreply.github.com>
2025-05-11 22:25:31 +0500

    Update redis.ts

--- server/src/services/redis.ts
+++ server/src/services/redis.ts
@@ -1,4 +1,7 @@
 import { createClient } from "redis";
+import { config } from "dotenv";
+
+config({path: "../.env"})
 
 const redisClient = createClient({
   url: process.env.REDIS_URL || "redis://localhost:6379",
